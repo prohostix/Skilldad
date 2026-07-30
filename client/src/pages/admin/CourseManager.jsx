d97c741bf82d69a6e1e79d2180328d0c37bbc05b
Rinsna <skilldad12@gmail.com>
2026-07-30 16:37:57 +0530

    Narrow the Provided By dropdown further

--- client/src/pages/admin/CourseManager.jsx
+++ client/src/pages/admin/CourseManager.jsx
@@ -393,7 +393,7 @@ const CourseManager = () => {
                                 onChange={(e) => setSearchQuery(e.target.value)}
                             />
                         </div>
-                        <div className="relative flex-1 md:w-36">
+                        <div className="relative flex-1 md:w-28">
                             <Filter className="absolute left-3 top-1/2 -translate-y-1/2 text-white/40 pointer-events-none" size={14} />
                             <select
                                 value={providerFilter}
